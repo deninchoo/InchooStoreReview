b4af4f712349d8bff97b1f5f6ef0a6753e678f5a
Deni <deni.pesic@inchoo.net>
2017-11-13 15:55:51 +0100

    Added security checks and removed redundant code

--- Inchoo/StoreReview/Controller/Index/CreatePost.php
+++ Inchoo/StoreReview/Controller/Index/CreatePost.php
@@ -33,6 +33,7 @@ class CreatePost extends Action
         $data = $this->getRequest()->getPostValue();
         $currentStore = $this->storeManagerInterface->getStore();
         $currentStoreId = $currentStore->getId();
+        $isLoggedIn = $this->session->isLoggedIn();
 
         /** @var \Magento\Backend\Model\View\Result\Redirect $resultRedirect */
         $resultRedirect = $this->resultRedirectFactory->create();
@@ -46,25 +47,26 @@ class CreatePost extends Action
             $model->setStoreId($currentStoreId);
             $model->setCustomerName($this->session->getCustomer()->getName());
 
-
-            try {
-                $this->dataResource->save($model);
-                $this->messageManager->addSuccessMessage('Store Review successfully saved');
-                $this->_objectManager->get('Magento\Backend\Model\Session')->setFormData(false);
-                if ($this->getRequest()->getParam('back')) {
-                    return $resultRedirect->setPath('*/*/edit', ['review_id' => $model->getId(), '_current' => true]);
+            if ($isLoggedIn) {
+                try {
+                    $this->dataResource->save($model);
+                    $this->messageManager->addSuccessMessage('Store Review successfully saved');
+                    $this->_objectManager->get('Magento\Backend\Model\Session')->setFormData(false);
+                    if ($this->getRequest()->getParam('back')) {
+                        return $resultRedirect->setPath('*/*/edit', ['review_id' => $model->getId(), '_current' => true]);
+                    }
+                    return $resultRedirect->setPath('*/*/');
+                } catch (\Magento\Framework\Exception\LocalizedException $e) {
+                    $this->messageManager->addErrorMessage($e->getMessage());
+                } catch (\RuntimeException $e) {
+                    $this->messageManager->addErrorMessage($e->getMessage());
+                } catch (\Exception $e) {
+                    $this->messageManager->addErrorMessage($e, __('Something went wrong while saving the Store Review.'));
                 }
-                return $resultRedirect->setPath('*/*/');
-            } catch (\Magento\Framework\Exception\LocalizedException $e) {
-                $this->messageManager->addErrorMessage($e->getMessage());
-            } catch (\RuntimeException $e) {
-                $this->messageManager->addErrorMessage($e->getMessage());
-            } catch (\Exception $e) {
-                $this->messageManager->addErrorMessage($e, __('Something went wrong while saving the Store Review.'));
-            }
 
-            $this->_getSession()->setFormData($data);
-            return $resultRedirect->setPath('*/*/edit', ['review_id' => $this->getRequest()->getParam('review_id')]);
+                $this->_getSession()->setFormData($data);
+                return $resultRedirect->setPath('*/*/edit', ['review_id' => $this->getRequest()->getParam('review_id')]);
+            }
         }
         return $resultRedirect->setPath('*/*/');
     }
